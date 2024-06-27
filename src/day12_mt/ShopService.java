73fb866d8cd246852c7ab8c9b4d77cab19c60405
a <jh5045@naver.com>
2024-06-27 11:34:59 +0900

    240627

--- src/day12_mt/ShopService.java
+++ src/day12_mt/ShopService.java
@@ -4,6 +4,10 @@ public class ShopService {
 
   private static ShopService instance = null;
 
+  private ShopService() {
+    System.out.println("객체가 생성되었습니다.");
+  }
+
   public static ShopService getInstance() {
     if (instance == null) {
       instance = new ShopService();
