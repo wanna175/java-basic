924a04ead5c159c4ac46fb25ccde06a7d4597672
a <jh5045@naver.com>
2024-07-19 15:51:56 +0900

    240719

--- src/Board0719/BoardExample.java
+++ src/Board0719/BoardExample.java
@@ -1,11 +1,12 @@
 package Board0719;
 
 import Board0719.Service.BoardService;
+import Board0719.Service.BoardServiceO;
 
 public class BoardExample {
 
   public static void main(String[] args) {
-    BoardService service = new BoardService();
+    BoardService service = new BoardServiceO();
     service.list();
   }
 
